6d2d0115b62587bbc2122493aea8489044692677
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-20 16:29:40 +0200

    printed members info on console

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,68 @@
+/*
+   MILESTONE 0
+*/
 // creo un array di oggetti per rappresentare i membri del team
-const membri = [
+const members = [
     {
-        Name: 'Wayne Barnett',
-        Role: 'Founder & CEO',
-        Image: 'wayne-barnett-founder-ceo.jpg'
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
     },
 
     {
-        Name: 'Angela Caroll',
-        Role: 'Chief Editor',
-        Image: 'angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
     },
 
     {
-        Name: 'Walter Gordon',
-        Role: 'Office Manager',
-        Image: 'walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
     },
 
     {
-        Name: 'Angela Lopez',
-        Role: 'Social Media Manager',
-        Image: 'angela-lopez-social-media-manager.jpg',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
 
     {
-        Name: 'Scott Estrada',
-        Role: 'Developer',
-        Image: 'scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
     },
 
     {
-        Name: 'Barbara Ramos',
-        Role: 'Grafic designer',
-        Image: 'barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Grafic designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
 ]
+console.log(members);
+
+/* 
+   /MILESTONE 0
+*/
+
+/* 
+   MILESTONE 1
+*/
+
+// stampo in console le informazioni di nome, ruolo e stringa della foto
+for (let i = 0; i < members.length; i++) {
+    const thisMember = members[i];
+    console.log(`Member ${i} Name : ` + thisMember.name);
+    console.log(`Member ${i} Role: ` + thisMember.role);
+    console.log(`Member ${i} Image: ` + thisMember.image);
+}
+
+/* 
+   /MILESTONE 1
+*/
+
+/* 
+   MILESTONE 2
+*/
+
+// stampo nella DOM le informazioni sottoforma di stringhe
