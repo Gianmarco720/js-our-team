20da45a12f20e309a91757170d2e80cc3c7a04ee
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-20 16:06:18 +0200

    add team object array

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+// creo un array di oggetti per rappresentare i membri del team
+const membri = [
+    {
+        Name: 'Wayne Barnett',
+        Role: 'Founder & CEO',
+        Image: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        Name: 'Angela Caroll',
+        Role: 'Chief Editor',
+        Image: 'angela-caroll-chief-editor.jpg',
+    },
+
+    {
+        Name: 'Walter Gordon',
+        Role: 'Office Manager',
+        Image: 'walter-gordon-office-manager.jpg',
+    },
+
+    {
+        Name: 'Angela Lopez',
+        Role: 'Social Media Manager',
+        Image: 'angela-lopez-social-media-manager.jpg',
+    },
+
+    {
+        Name: 'Scott Estrada',
+        Role: 'Developer',
+        Image: 'scott-estrada-developer.jpg',
+    },
+
+    {
+        Name: 'Barbara Ramos',
+        Role: 'Grafic designer',
+        Image: 'barbara-ramos-graphic-designer.jpg',
+    },
+]
